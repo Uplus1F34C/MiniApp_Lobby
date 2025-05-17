2d482b11275baed39b858f4fde1c702eb5e5e6f0
U+1F34C <uplus1f34c.study@gmail.com>
2025-05-17 17:30:02 +0500

    0.6.2

--- Web/Tree/script.js
+++ Web/Tree/script.js
@@ -414,7 +414,9 @@ async function initializeTopics() {
             // Добавляем изображение, если оно есть
             if (topic.img) {
                 const img = document.createElement('img');
-                img.src = `/Web/img/TOPIC/${topic.img}`;
+                img.src = `img/TOPIC/${topic.img}`;
+                console.log(img.src);
+                console.log(i`img/TOPIC/${topic.img}`);
                 topicBtn.appendChild(img);
             }
 
